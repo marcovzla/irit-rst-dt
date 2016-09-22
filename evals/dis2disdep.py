f185f3af6fe0c1663e22bb2c99421b0ed66b895e
moreymat <mathieu.morey@gmail.com>
2016-09-22 15:54:22 +0200

    ENH added support for DPLP in eval

--- evals/dis2disdep.py
+++ evals/dis2disdep.py
@@ -8,20 +8,18 @@ TODO
 """
 from __future__ import absolute_import, print_function
 import argparse
-from collections import defaultdict
-from glob import glob
 import os
 
-from educe.annotation import Span
 from educe.corpus import FileId
 from educe.learning.disdep_format import dump_disdep_files
-from educe.rst_dt.annotation import Node, RSTTree
 from educe.rst_dt.codra import load_codra_output_files
-from educe.rst_dt.corpus import Reader
+from educe.rst_dt.corpus import Reader, RstRelationConverter
 from educe.rst_dt.deptree import RstDepTree
 from educe.rst_dt.feng import load_feng_output_files
 from educe.rst_dt.rst_wsj_corpus import (DOUBLE_FOLDER, TEST_FOLDER,
                                          TRAIN_FOLDER)
+
+from .ji import load_ji_dtrees
 
 
 # original RST corpus
@@ -29,12 +27,19 @@ RST_CORPUS = os.path.join('/home/mmorey/corpora/rst_discourse_treebank/data')
 RST_MAIN_TRAIN = os.path.join(RST_CORPUS, TRAIN_FOLDER)
 RST_MAIN_TEST = os.path.join(RST_CORPUS, TEST_FOLDER)
 RST_DOUBLE = os.path.join(RST_CORPUS, DOUBLE_FOLDER)
+
+# relation converter (fine- to coarse-grained labels)
+RELMAP_FILE = os.path.join('/home/mmorey/melodi/educe',
+                           'educe', 'rst_dt',
+                           'rst_112to18.txt')
+REL_CONV = RstRelationConverter(RELMAP_FILE).convert_tree
+
 # output of Joty's parser
 OUT_JOTY = os.path.join('/home/mmorey/melodi/rst/joty/Doc-level/')
 # output of Feng & Hirst's parser
 OUT_FENG = os.path.join('/home/mmorey/melodi/rst/feng_hirst/tmp/')
 # output of Ji's parser
-OUT_JI = os.path.join('/home/mmorey/melodi/rst/ji_eisenstein/test_input')
+OUT_JI = os.path.join('/home/mmorey/melodi/rst/ji_eisenstein/DPLP/data/docs/test/')
 
 
 def main():
@@ -111,88 +116,7 @@ def main():
         if corpus_split != 'test':
             raise ValueError("The output of Ji & Eisenstein's parser is "
                              "available for the 'test' split only")
-        # * load the text of the EDUs
-        # FIXME get the text of EDUs from the .merge files
-        corpus_dir = RST_MAIN_TEST
-        reader_true = Reader(corpus_dir)
-        ctree_true = reader_true.slurp()
-        doc_edus = {k.doc: ct_true.leaves() for k, ct_true
-                    in ctree_true.items()}
-        # * for each doc, load the predicted spans from the .brackets
-        ctree_pred = dict()
-        files_pred = os.path.join(OUT_JI, '*.brackets')
-        for f_pred in sorted(glob(files_pred)):
-            doc_name = os.path.splitext(os.path.basename(f_pred))[0]
-            edus = {i: e for i, e in enumerate(doc_edus[doc_name], start=1)}
-            origin = FileId(doc_name, None, None, None)
-            # read spans
-            spans_pred = defaultdict(list)  # predicted spans by length
-            with open(f_pred) as f:
-                for line in f:
-                    # FIXME use a standard module: ast or pickle?
-                    # drop surrounding brackets + opening bracket of edu span
-                    line = line.strip()[2:-1]
-                    edu_span, nuc_rel = line.split('), ')
-                    edu_span = tuple(int(x) for x in edu_span.split(', '))
-                    nuc, rel = nuc_rel.split(', ')
-                    edu_span_len = edu_span[1] - edu_span[0]
-                    spans_pred[edu_span_len].append((edu_span, nuc, rel))
-            # bottom-up construction of the RST ctree
-            # left_border -> list of RST ctree fragments, sorted by len
-            tree_frags = defaultdict(list)
-            for span_len, spans in sorted(spans_pred.items()):
-                for edu_span, nuc, rel in spans:
-                    children = []
-                    edu_beg, edu_end = edu_span
-                    if edu_beg == edu_end:
-                        # leaf node
-                        txt_span = edus[edu_beg].span
-                    else:
-                        # internal node
-                        # * get the children (subtrees)
-                        edu_cur = edu_beg
-                        while edu_cur < edu_end:
-                            kid_nxt = tree_frags[edu_cur][-1]
-                            children.append(kid_nxt)
-                            edu_cur = kid_nxt.label().edu_span[1] + 1
-                        # compute properties of this node
-                        txt_span = Span(children[0].label().span.char_start,
-                                        children[-1].label().span.char_end)
-                    # build node and RSTTree fragment
-                    node = Node(nuc, edu_span, txt_span, rel,
-                                context=None)  # TODO context?
-                    tree_frags[edu_beg].append(
-                        RSTTree(node, children, origin=origin))
-            # build the top node
-            edu_nums = sorted(edus.keys())
-            edu_span = (edu_nums[0], edu_nums[-1])
-            print(doc_name, edu_span)
-            children = []
-            edu_beg, edu_end = edu_span
-            edu_cur = edu_beg
-            while edu_cur < edu_end:
-                print(edu_cur)
-                kid_nxt = tree_frags[edu_cur][-1]
-                children.append(kid_nxt)
-                edu_cur = kid_nxt.label().edu_span[1] + 1
-            txt_span = Span(children[0].label().span.char_start,
-                            children[-1].label().span.char_end)
-            node = Node(nuc, edu_span, txt_span, 'Root', context=None)
-            tree_frags[edu_beg].append(
-                RSTTree(node, children, origin=origin))
-            # now we should have a spanning ctree
-            ct_pred = tree_frags[1][-1]
-            # DEBUG
-            print(sorted(edus.keys())[0],
-                  sorted(edus.keys())[-1])
-            print(ct_pred.label().edu_span)  # RESUME HERE
-            print(sorted(tree_frags.items()))
-            # end DEBUG
-            assert ct_pred.label().edu_span == (sorted(edus.keys())[0],
-                                                sorted(edus.keys())[-1])
-            ctree_pred[doc_name] = ct_pred
-            
-        raise NotImplementedError("Output of Ji's parser")
+        dtrees = load_ji_dtrees(OUT_JI, REL_CONV)
     # do dump
     dump_disdep_files(dtrees.values(), out_dir)
 
